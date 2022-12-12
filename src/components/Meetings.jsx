b52c32bddad7390793a23447342f46df14300c55
vince <vincetanan@gmail.com>
2022-12-12 13:45:01 +0800

    calendar application

--- src/components/Meetings.jsx
+++ src/components/Meetings.jsx
@@ -12,10 +12,6 @@ const Meetings = () => {
 
   let { current_sorted } = meetings;
 
-  const onOpenModal = () => {
-    dispatch({ type: actions.TOGGLE_MEETING_MODAL });
-  };
-
   const onDeleteCard = (id) => {
     fetch(`http://localhost:3500/meetings/${id}`, {
       method: 'DELETE',
